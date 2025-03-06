30b4be0dc859c23cac45b9491af5b566c5b7deb5
leonardonels <golenmatrix2.0@gmail.com>
2025-03-06 12:42:25 +0100

    first idea for main implementation

--- floratech_hub/database/tinydb_manager.py
+++ floratech_hub/database/tinydb_manager.py
@@ -1,11 +1,21 @@
 from tinydb import TinyDB, Query
 
 class DatabaseManager:
-    def __init__(self, db_path="floratech_hub.json"):
+    def __init__(self, db_path="/var/lib/floratech/db.json"):
         self.db = TinyDB(db_path)
 
-    def save_message(self, message):
-        self.db.insert({"message": message})
+    def closedb(self):
+        self.db.close()
 
-    def get_messages(self):
-        return self.db.all()
+    def createtable(self, tablename):
+        return self.db.table(tablename)
+
+    def insert(table, elem):
+        table.insert(elem)
+
+    def printtable(table):
+        for sample in table.all():
+            print(sample)
+
+    def cleartable(table):
+        table.purge()
--- floratech_hub/main.py
+++ floratech_hub/main.py
@@ -1,19 +1,94 @@
+import asyncio
 from floratech_hub.lora.lora import LoRaModule
 from floratech_hub.database.tinydb_manager import DatabaseManager
 from floratech_hub.server.server_api import ServerAPI
 import config
 
-def main():
-    lora = LoRaModule()
+class AsyncLoRaModule:
+    def __init__(self):
+        self.lora = LoRaModule()
+
+    async def receive(self):
+        while True:
+            message = await self.lora.receive()
+            if message:
+                return message
+
+async def timed_callback(server, db):
+    while True:
+        await asyncio.sleep(3600)  # 1 hour delay
+        messages = db.get_messages_to_send()  # Assume this method fetches messages to send
+        for message in messages:
+            server.send_data({"message": message})
+
+async def main():
+    lora = AsyncLoRaModule()
     db = DatabaseManager()
     server = ServerAPI(config.SERVER_URL)
 
+    # Create tables if not present (if the software was restarted and the db already exists there is no need to erase the old one)
+    # One table for sensors and actuators
+        # id
+        # role (sensor or actuator)
+        # last ping
+        # garden
+    # One table to record moistures
+        # id?
+        # timestamp
+        # moisture
+        # sensor from
+        # garden
+
+    # Start the timed callback task for periodic updates
+    update_task = asyncio.create_task(timed_callback(server, db))
+
     while True:
-        message = lora.receive()
+        message = await lora.receive()
+
         if message:
-            print(f"Messaggio ricevuto: {message}")
-            db.save_message(message)
-            server.send_data({"message": message})
+            # Look for the header of the message
+            if message.startswith("sensor"):
+                if not db.is_sensor_configured(message):
+                    # Configure sensor by answering his ID
+                    response = {"id": message}
+                    server.send_data(response)
+                    
+                    # Get updated sensor configuration from server
+                    updated_config = await server.get_sensor_config(message)
+                    db.update_sensor_configuration(updated_config)
+                
+                else:
+                    # Save moisture into db
+                    db.save_moisture(message)
+
+            elif message.startswith("actuator"):
+                if not db.is_actuator_configured(message):
+                    # Configure actuator by answering his ID
+                    response = {"id": message}
+                    server.send_data(response)
+                    
+                    # Get updated sensor configuration from server
+                    updated_config = await server.get_sensor_config(message)
+                    db.update_actuator_configuration(updated_config)
+                
+                else:
+                    # Read needed water in mm from db
+                    needed_water = db.get_needed_water_in_mm()
+                    
+                    # Compute the time the pump is required to be used
+                    if needed_water == 0:
+                        # Answer with off state
+                        response = {"state": "off"}
+                    else:
+                        # Compute the time needed
+                        duration = compute_pump_duration(needed_water)
+                        response = {"state": "on", "duration": duration}
+                    
+                    # Send response to server
+                    server.send_data(response)
+
+        # End this loop iteration by creating a new task waiting for another message
+        lora_task = asyncio.create_task(lora.receive())
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
